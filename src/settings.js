5056b88046a4b45422b0503c8b90bd7fc15e5673
kyuu2601-star <kyuu2601@gmail.com>
2026-06-25 17:59:48 +0700

    Delete src directory

--- src/settings.js
+++
@@ -1,1 +0,0 @@
-window._CCSettings={platform:"web-mobile",groupList:["MainMenu","BattleStage"],collisionMatrix:[[true],[false,true]],hasResourcesBundle:false,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/Scenes/MainMenu.fire",orientation:"",jsList:[]};
